a9fadf08f3cfd43756ac067c208e590a7312d683
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-09-11 16:44:25 -0600

    updated the design of the project cards

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        <h2 className='flex justify-center text-xl mt-[20px]'>Overview</h2>
+        <h2 className='flex justify-center text-xl mt-20'>Overview</h2>
         <p className='mt-[10px]'>Welcome to my Personal Software Portfolio, a curated collection of projects that embodies a journey of learning, problem-solving, and technological exploration. This portfolio is a testament to the dedication and expertise of its creator, showcasing a wide spectrum of skills and knowledge acquired over time.</p>
     </div>
   )
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,78 +1,85 @@
 'use client'
 
-import React from 'react'
-import { projects } from '@/constants'
-import Image from 'next/image'
+import { projects } from "@/constants"
+import Image from "next/image"
 
-const ProjectCard = ({ index, name, description, tags, image, source_code, live_demo }) => {
-    return (
-    <div className="relative h-[250px] border-solid border-2 border-black rounded-2xl p-5">
-        <Image 
-            src={image}
-            alt={name}
-            width={100}
-            height={100}
-            className="object-contain"
-        />
+const ProjectCard = ({ index, name, description, tags, image,  source_code, live_demo }) => {
+    return (  
+        <div className="border-solid border-2 border-black rounded-2xl">
+            <div className='p-5 rounded-2xl sm:w-[360px] w-full'>
+                <div className="relative w-full h-[230px]">
+                    <Image
+                        src={image}
+                        alt={name}
+                        width={100}
+                        height={100}
+                        className="w-full h-full object-cover rounded-2xl"
+                    />
 
-        <div className="absolute inset-0 flex justify-end m-3">
-            <div
-                onClick={() => window.open(live_demo, '_blank')}
-                className="w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-                <Image
-                    src='/chrome.png'
-                    alt='domain'
-                    width={100}
-                    height={100}
-                    className="w-3/4 h-3/4 object-contain bg-black rounded-full"
-                />
+                    <div className="absolute inset-0 flex justify-end m-3">
+                        <div
+                        onClick={() => window.open(live_demo, '_blank')}
+                        className="w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                        >
+                        <Image
+                            src='/chrome.png'
+                            alt='domain'
+                            width={100}
+                            height={100}
+                            className="w-3/4 h-3/4 object-contain bg-black rounded-full"
+                        />
+                        </div>  
+
+                        <div
+                        onClick={() => window.open(source_code, '_blank')}
+                        className="w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                        >
+                        <Image 
+                            src='/github.png'
+                            alt='github'
+                            width={100}
+                            height={100}
+                            className="w-3/4 h-3/4 object-contain bg-black rounded-full"
+                        />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mt-5">
+                    <h3 className="font-bold text-[24px]">{name}</h3>
+                    <p className="mt-2 text-secondary text-[14px]">{description}</p>
+                </div>
+
+                <div className="mt-4 flex flex-wrap gap-2">
+                    {tags.map((tag) => (
+                        <p key={tag.name} className='text-[14px]'>
+                            #{tag.name}
+                        </p>
+                    ))}
+                </div>
             </div>
-            <div
-                onClick={() => window.open(source_code, '_blank')}
-                className="w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-                <Image
-                    src='/github.png'
-                    alt='github'
-                    width={100}
-                    height={100}
-                    className="w-3/4 h-3/4 object-contain bg-black rounded-full"
-                />
-            </div>
         </div>
-
-        <div className="mt-5">
-            <h3 className="font-bold text-[24px]">{name}</h3>
-            <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
-
-        <div className="mt-4 flex flex-wrap gap-2">
-            {tags.map((tag) => (
-                <p key={tag.name} className='text-[14px]'>
-                    #{tag.name}
-                </p>
-            ))}
-        </div>
-    </div>
     )
 }
 
 const Projects = () => {
-  return (
-    <div>
-        <h2 className='flex justify-center text-xl mt-[20px]'>Projects</h2>
-        <div className='mt-10 flex flex-wrap gap-7'>
-            {projects.map((project, index) => (
-                <ProjectCard 
-                    key={`project-${index}`}
-                    index={index}
-                    {...project}
-                />
-            ))}
-        </div>
-    </div>
-  )
+    return (
+        <>
+            <div>
+                <h2 className='flex justify-center text-xl mt-20'>Projects</h2>
+            </div>
+
+            <div className="mt-10 flex flex-wrap gap-7">
+                {projects.map((project, index) => (
+                    <ProjectCard 
+                        key={`project-${index}`}
+                        index={index}
+                        {...project}
+                    />
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default Projects
